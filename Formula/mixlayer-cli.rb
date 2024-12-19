47042901978f036e2d201ec20a8f6d44c490486d
Zack Angelo <zackangelo@gmail.com>
2024-12-19 12:18:48 -0600

    Update mixlayer-cli.rb
    correct sha sum

--- Formula/mixlayer-cli.rb
+++ Formula/mixlayer-cli.rb
@@ -5,7 +5,7 @@ class MixlayerCli < Formula
 
     if OS.mac? && Hardware::CPU.arm?
         url "https://github.com/mixlayer/homebrew-tap/releases/download/v#{version}/mixlayer-cli.tar.gz"
-        sha256 "55e7a97f582f270aa54e9a6acff86dc75efddf5feb6f4baad801f8a8027ac363"
+        sha256 "b80a7c2e654f01a2ec0d2dd75c712fa8df19087d247f69d87702ca2db81bd12d"
     end
 
     def install
